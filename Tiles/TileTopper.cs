e205aeeea72ec447a31c8389dc9d6093eddb124e
Tanner Mann <50087680+Crazytm24@users.noreply.github.com>
2025-12-14 17:19:39 -0700

    Clean up TileTopper by removing unused methods
    Removed empty Start and Update methods from TileTopper.

--- Tiles/TileTopper.cs
+++ Tiles/TileTopper.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class TileTopper : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    // This component will be needed to allow the tile topper to be more dynamic
 }
